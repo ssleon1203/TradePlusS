57e175dc5a10d7bb89a48e7efbd72e134247b659
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2020-07-04 13:50:59 -0500

    Vista Ventas en Vendedor arreglado

--- mTradePlus/src/main/java/pe/edu/upc/mTradePlus/model/controller/VentaController.java
+++ mTradePlus/src/main/java/pe/edu/upc/mTradePlus/model/controller/VentaController.java
@@ -1,15 +1,23 @@
 package pe.edu.upc.mTradePlus.model.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import pe.edu.upc.mTradePlus.model.entity.Cliente;
 import pe.edu.upc.mTradePlus.model.entity.Compra;
+import pe.edu.upc.mTradePlus.model.entity.Producto;
+import pe.edu.upc.mTradePlus.model.entity.Suscripcion;
 import pe.edu.upc.mTradePlus.model.service.ClienteService;
 import pe.edu.upc.mTradePlus.model.service.CompraService;
 import pe.edu.upc.mTradePlus.model.service.ProductoService;
@@ -38,4 +46,5 @@ public class VentaController {
 		}
 		return "/venta/startVenta";
 	}
+
 }
