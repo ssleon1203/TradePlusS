e5d45ac91b8cd64bcaf01560132a04484b784c99
user.email <josenear1@gmail.com>
2020-07-04 13:50:34 -0500

    aporte1

--- mTradePlus/src/main/java/pe/edu/upc/mTradePlus/model/repository/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.mTradePlus.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.mTradePlus.model.entity.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long>{
-
-}
--- mTradePlus/src/main/java/pe/edu/upc/mTradePlus/model/service/SuscripcionService.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.mTradePlus.model.service;
-
-import pe.edu.upc.mTradePlus.model.entity.Suscripcion;
-
-public interface SuscripcionService extends CrudService<Suscripcion, Integer>{
-
-}
